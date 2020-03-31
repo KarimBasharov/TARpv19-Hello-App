a1e5f9942e6d8c2f174fe91bd0131af1aa7c51d7
Karim <karimbasharov@gmail.com>
2020-03-31 14:22:19 +0300

    Ask User Name

--- TARpv19/Program.cs
+++ TARpv19/Program.cs
@@ -6,7 +6,12 @@ namespace TARpv19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please, enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello" + name + "!");
+
+
 
             Console.ReadLine();
         }
